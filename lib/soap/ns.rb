fc932877197c1b1ce819769c275eed046eaca6bf
Anton Sozontov <asozontov@at-consulting.ru>
2011-09-27 14:53:47 +0400

    add xsd/wsdl/soap libs

---
+++ lib/soap/ns.rb
@@ -0,0 +1,39 @@
+# encoding: ASCII-8BIT
+# SOAP4R - SOAP Namespace library
+# Copyright (C) 2000-2007  NAKAMURA, Hiroshi <nahi@ruby-lang.org>.
+
+# This program is copyrighted free software by NAKAMURA, Hiroshi.  You can
+# redistribute it and/or modify it under the same terms of Ruby's license;
+# either the dual license version in 2003, or any later version.
+
+
+require 'xsd/datatypes'
+require 'xsd/ns'
+require 'soap/soap'
+
+
+module SOAP
+
+## Rubyjedi: Provide these (deprecated) constants for backward compatibility with older ActionWebService
+SOAPNamespaceTag = 'env'
+XSDNamespaceTag  = 'xsd'
+XSINamespaceTag  = 'xsi'
+
+class NS < XSD::NS
+  KNOWN_TAG = XSD::NS::KNOWN_TAG.dup.update(
+    SOAP::EnvelopeNamespace => 'env'
+  )
+
+  def initialize(tag2ns = nil)
+    super(tag2ns)
+  end
+
+private
+
+  def default_known_tag
+    KNOWN_TAG
+  end
+end
+
+
+end
---
+++ lib/soap/version.rb
@@ -0,0 +1,9 @@
+# encoding: ASCII-8BIT
+module SOAP
+  module VERSION #:nodoc:
+    MAJOR = 1
+    MINOR = 5
+    TINY  = 8
+    STRING = [MAJOR, MINOR, TINY].join('.')
+  end
+end
