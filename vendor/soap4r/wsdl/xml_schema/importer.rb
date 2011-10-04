6527d098a50c511c8938318da6665324437f5bb5
Miles Z. Sterrett <miles.sterrett@gmail.com>
2011-10-05 01:50:11 +0400

    fix require netHttpClient

--- vendor/soap4r/wsdl/xml_schema/importer.rb
+++ vendor/soap4r/wsdl/xml_schema/importer.rb
@@ -90,7 +90,7 @@ private
         @web_client = HTTPAccess2::Client
       rescue LoadError
         warn("Loading http-access2 failed.  Net/http is used.") if $DEBUG
-        require 'soap/netHttpClient'
+        require 'soap/net_http_client'
         @web_client = ::SOAP::NetHttpClient
       end
     end
